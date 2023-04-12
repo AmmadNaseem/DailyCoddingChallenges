6bc636fe4134676334c2fd37ea6785f811cd146e
Aamad Naseem <ammadnaseem878@gmail.com>
2023-04-12 16:47:02 +0500

    Print & Remove duplicate characters of String

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace DailyCoddingChallenges
+﻿using static System.Net.Mime.MediaTypeNames;
+using System;
+using DailyCoddingChallenges.Problems;
+
+
+namespace DailyCoddingChallenges
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("************Daily Coding Problems************");
+            Console.WriteLine("Day1: Remove Duplicate Character in a String:");
+
+            Console.Write("Enter String:");
+            string str = Console.ReadLine();
+
+            string newStr = StringUtils.RemoveDuplicates(str);
+
+            Console.WriteLine($"Final String is:{newStr}");
+
+            string duplicateChar = StringUtils.PrintDuplicate(str);
+
+            Console.WriteLine($"Duplicated Characters in String is:{duplicateChar}");
+
+
         }
     }
 }
