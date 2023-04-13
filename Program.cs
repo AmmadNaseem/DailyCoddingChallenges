bd134b4daa550036101524de5ae655a85a865164
Aamad Naseem <ammadnaseem878@gmail.com>
2023-04-13 16:38:41 +0500

    String Anagram

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace DailyCoddingChallenges
         static void Main(string[] args)
         {
             Console.WriteLine("************Daily Coding Problems************");
+
+            Console.WriteLine("Day2: Two strings are anagram to each other:");
+
+            Console.Write("Enter First String:");
+            string str1 = Console.ReadLine();
+
+            Console.Write("Enter 2nd String:");
+            string str2 = Console.ReadLine();
+
+
+            StringUtils.Anagram(str1, str2);
+
         }
     }
 }
