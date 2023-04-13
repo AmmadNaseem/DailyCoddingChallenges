6a38184fb91d79e42cd53205c62e19480f34250e
Aamad Naseem <ammadnaseem878@gmail.com>
2023-04-13 16:48:22 +0500

    changed branched name

--- Program.cs
+++ Program.cs
@@ -11,6 +11,20 @@ namespace DailyCoddingChallenges
         {
             Console.WriteLine("************Daily Coding Problems************");
 
+            Console.WriteLine("************Daily Coding Problems************");
+            Console.WriteLine("Day1: Remove Duplicate Character in a String:");
+
+            Console.Write("Enter String:");
+            string str = Console.ReadLine();
+
+            string newStr = StringUtils.RemoveDuplicates(str);
+
+            Console.WriteLine($"Final String is:{newStr}");
+
+            string duplicateChar = StringUtils.PrintDuplicate(str);
+
+            Console.WriteLine($"Duplicated Characters in String is:{duplicateChar}");
+
             Console.WriteLine("Day2: Two strings are anagram to each other:");
 
             Console.Write("Enter First String:");
